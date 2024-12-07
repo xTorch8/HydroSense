0b86e192eeb56014b5b4792acaa2a88d2f88ac1b
xTorch8 <esantosa.es@gmail.com>
2024-12-07 14:30:30 +0700

    evan - Add navy and biru5 colors in Tailwind CSS

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				navy: "#0D1232",
+				biru5: "#445EF2",
+			},
+		},
+	},
+	plugins: [],
+};
