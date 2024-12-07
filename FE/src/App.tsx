dfde9b1d051f566aeb27fbd874544d96ba3ca1a5
xTorch8 <esantosa.es@gmail.com>
2024-12-07 22:20:28 +0700

    evan - Add Product Detail Page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import ProductListPage from "./pages/Products/ProductListPage";
+import ProductDetailPage from "./pages/Products/ProductDetailPage";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="products" element={<ProductListPage />} />
+				<Route path="products/detail" element={<ProductDetailPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
