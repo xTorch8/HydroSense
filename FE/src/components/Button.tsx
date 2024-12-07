e16b22157efc7874154fdb9335c48a664003dbdc
xTorch8 <esantosa.es@gmail.com>
2024-12-07 19:52:46 +0700

    evan - Add align props in ButtonType

--- FE/src/components/Button.tsx
+++ FE/src/components/Button.tsx
@@ -1,15 +1,24 @@
 import ButtonTypes from "../types/ButtonType";
 
 const Button = (props: ButtonTypes) => {
+	let align = "mx-auto ";
+	if (props.align == "left") {
+		align += "md:float-left";
+	} else if (props.align == "right") {
+		align += "md:float-right";
+	}
+
 	if (props.isPrimary == null || props.isPrimary == true) {
+		let style = "bg-biru5 rounded-lg block px-4 py-2 text-white border-biru1 border-2 font-semibold " + align;
 		return (
-			<button onClick={props.onClick} className="bg-biru5 rounded-lg block mx-auto px-4 py-2 text-white border-biru1 border-2 font-semibold">
+			<button onClick={props.onClick} className={style}>
 				{props.text}
 			</button>
 		);
 	} else {
+		let style = "bg-biru2 rounded-lg block mx-auto px-4 py-2 text-black font-semibold " + align;
 		return (
-			<button onClick={props.onClick} className="bg-biru2 rounded-lg block mx-auto px-4 py-2 text-black font-semibold">
+			<button onClick={props.onClick} className={style}>
 				{props.text}
 			</button>
 		);
--- FE/src/types/ButtonType.ts
+++ FE/src/types/ButtonType.ts
@@ -2,6 +2,7 @@ type ButtonTypes = {
 	text: string;
 	isPrimary?: boolean;
 	onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	align?: "left" | "center" | "right"
 };
 
 export default ButtonTypes;
