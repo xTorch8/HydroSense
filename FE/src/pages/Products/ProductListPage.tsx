137533ee723f634656ce5ad354a61968eca6eeac
xTorch8 <esantosa.es@gmail.com>
2024-12-15 14:43:24 +0700

    evan - Revamp Product List page

--- FE/src/pages/Products/ProductListPage.tsx
+++ FE/src/pages/Products/ProductListPage.tsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router";
 import Navbar from "../../components/Navbar";
-import Cards from "../../components/Cards";
+// import Cards from "../../components/Cards";
 import Button from "../../components/Button";
+import ProductItem from "../../components/ProductItem";
 
 const ProductListPage = () => {
 	let navigate = useNavigate();
@@ -10,18 +11,24 @@ const ProductListPage = () => {
 		{
 			id: 1,
 			name: "Mineral Cup",
+			description:
+				"Aqua Mineral Cup memberikan kemudahan untuk kebutuhan hidrasi Anda. Dikemas secara higienis dengan air dari sumber terbaik, menjaga kualitas dan kemurniannya hingga siap dinikmati.",
 			image:
 				"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//110/MTA-68556755/aqua_aqua_air_mineral_gelas_cup_220ml_-1_box_48_cup-_full01_oopjgexh.jpg",
 		},
 		{
 			id: 2,
 			name: "Galon",
+			description:
+				"Aqua Mineral Cup memberikan kemudahan untuk kebutuhan hidrasi Anda. Dikemas secara higienis dengan air dari sumber terbaik, menjaga kualitas dan kemurniannya hingga siap dinikmati.",
 			image:
 				"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//110/MTA-68556755/aqua_aqua_air_mineral_gelas_cup_220ml_-1_box_48_cup-_full01_oopjgexh.jpg",
 		},
 		{
 			id: 3,
 			name: "Mineral Bottle",
+			description:
+				"Aqua Mineral Cup memberikan kemudahan untuk kebutuhan hidrasi Anda. Dikemas secara higienis dengan air dari sumber terbaik, menjaga kualitas dan kemurniannya hingga siap dinikmati.",
 			image:
 				"https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//110/MTA-68556755/aqua_aqua_air_mineral_gelas_cup_220ml_-1_box_48_cup-_full01_oopjgexh.jpg",
 		},
@@ -40,13 +47,14 @@ const ProductListPage = () => {
 				</div>
 			</div>
 
-			<div className="grid grid-cols-1 md:grid-cols-3 gap-6 mx-auto w-1/2 mt-8 md:w-4/5">
+			<div className="mx-auto w-1/2 mt-8 md:w-4/5">
 				{dummyData.map((item) => {
 					return (
-						<Cards
+						<ProductItem
+							id={item.id.toString()}
+							title={item.name}
+							description={item.description}
 							imagePath={item.image}
-							text={item.name}
-							key={item.id}
 							onClick={() => {
 								navigate("/products/detail", {
 									state: {
@@ -57,6 +65,20 @@ const ProductListPage = () => {
 								});
 							}}
 						/>
+						// <Cards
+						// 	imagePath={item.image}
+						// 	text={item.name}
+						// 	key={item.id}
+						// 	onClick={() => {
+						// 		navigate("/products/detail", {
+						// 			state: {
+						// 				id: item.id,
+						// 				name: item.name,
+						// 				image: item.image,
+						// 			},
+						// 		});
+						// 	}}
+						// />
 					);
 				})}
 			</div>
