92a58cc98476a1921d73d0b36454a8779b6df3db
xTorch8 <esantosa.es@gmail.com>
2024-12-15 16:18:12 +0700

    evan - Implement context in App

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import ProductListPage from "./pages/Products/ProductListPage";
 import ProductDetailPage from "./pages/Products/ProductDetailPage";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="products" element={<ProductListPage />} />
-				<Route path="products/detail" element={<ProductDetailPage />} />
-			</Routes>
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="products" element={<ProductListPage />} />
+					<Route path="products/detail" element={<ProductDetailPage />} />
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
 	);
 }
 
