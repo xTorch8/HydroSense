55e4c20d0260d2ae6ce411d1a0424cedd7b09fc0
Brian Susanto <alexander.susanto001@binus.ac.id>
2024-12-16 23:22:59 +0700

    Delete Routing untuk Dashboard Leaderboard Page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import ProductListPage from "./pages/Products/ProductListPage";
 import ProductDetailPage from "./pages/Products/ProductDetailPage";
-import DashboardPage from "./pages/Dashboard/DashboardPage";
 import { AuthProvider } from "./context/AuthContext";
 import AddProductPage from "./pages/Products/AddProductPage";
 
@@ -13,7 +12,6 @@ function App() {
 					<Route path="products" element={<ProductListPage />} />
 					<Route path="products/detail" element={<ProductDetailPage />} />
 					<Route path="products/add" element={<AddProductPage />} />
-					<Route path="dashboard" element={<DashboardPage />} />
 				</Routes>
 			</BrowserRouter>
 		</AuthProvider>
