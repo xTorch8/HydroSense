2e9f3586df97c4610b6496b1c3c727d2aa07c863
Brian Susanto <alexander.susanto001@binus.ac.id>
2024-12-16 20:26:20 +0700

    Brian - Add Routing to Dashboard Page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import ProductListPage from "./pages/Products/ProductListPage";
 import ProductDetailPage from "./pages/Products/ProductDetailPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 				<Routes>
 					<Route path="products" element={<ProductListPage />} />
 					<Route path="products/detail" element={<ProductDetailPage />} />
+					<Route path="dashboard" element={<DashboardPage />} />
 				</Routes>
 			</BrowserRouter>
 		</AuthProvider>
