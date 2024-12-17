9957ad385ab1c008dab45b78b5b4978b75f78302
Brian Susanto <alexander.susanto001@binus.ac.id>
2024-12-18 02:23:42 +0700

    Update App.tsx to have Routing to Dashboard

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import ProductListPage from "./pages/Products/ProductListPage";
 import ProductDetailPage from "./pages/Products/ProductDetailPage";
-// import DashboardPage from "./pages/Dashboard/DashboardPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 import { AuthProvider } from "./context/AuthContext";
 import AddProductPage from "./pages/Products/AddProductPage";
 import LoginPage from "./pages/Auth/LoginPage";
@@ -17,7 +17,7 @@ function App() {
 					<Route path="products" element={<ProductListPage />} />
 					<Route path="products/detail" element={<ProductDetailPage />} />
 					<Route path="products/add" element={<AddProductPage />} />
-					{/* <Route path="dashboard" element={<DashboardPage />} /> */}
+					<Route path="dashboard" element={<DashboardPage />} />
 				</Routes>
 			</BrowserRouter>
 		</AuthProvider>
