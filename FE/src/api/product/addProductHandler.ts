aa96eade06e4c9e56aec12a8caddfc71dd17b10a
xTorch8 <esantosa.es@gmail.com>
2024-12-18 20:07:30 +0700

    evan - Fix addProductHandler

--- FE/src/api/product/addProductHandler.ts
+++ FE/src/api/product/addProductHandler.ts
@@ -10,35 +10,33 @@ const addProductHandler = async (request: addProductRequest) => {
 		formData.append("Description", request.description);
 
 		if (request.image) {
-			formData.append("image", request.image);
+			formData.append("Image", request.image);
 		}
 
 		const waterData = JSON.stringify({
 			pH: request.waterData.pH,
-			lead: request.waterData.lead,
-			odor: request.waterData.odor,
-			totalDissolvedSolids: request.waterData.totalDissolvedSolids,
-			iron: request.waterData.iron,
-			turbidity: request.waterData.turbidity,
-			sulfate: request.waterData.sulfate,
-			nitrate: request.waterData.nitrate,
-			flouride: request.waterData.flouride,
-			chlorine: request.waterData.chlorine,
-			chloride: request.waterData.chloride,
-			copper: request.waterData.copper,
-			manganese: request.waterData.manganese,
+			Iron: request.waterData.iron,
+			Nitrate: request.waterData.nitrate,
+			Chloride: request.waterData.chloride,
+			Lead: request.waterData.lead,
+			Turbidity: request.waterData.turbidity,
+			Fluoride: request.waterData.flouride,
+			Copper: request.waterData.copper,
+			Odor: request.waterData.odor,
+			Sulfate: request.waterData.sulfate,
+			Chlorine: request.waterData.chlorine,
+			Manganese: request.waterData.manganese,
+			Total_Dissolved_Solids: request.waterData.totalDissolvedSolids,
 		});
 
 		formData.append("water_data", waterData);
 
-		const response = await axios.post(`${API_PATH}/product/save`, formData, {
+		const response = await axios.post(`${API_PATH}/product/save/`, formData, {
 			headers: {
 				Authorization: `Bearer ${request.token}`,
-				// "Content-Type": "multipart/form-data",
+				"Content-Type": "multipart/form-data",
 			},
 		});
-
-		console.log(response);
 
 		return response.data;
 	} catch (e) {
