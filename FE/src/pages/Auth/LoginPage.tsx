62ee4a2287b3ea212e1a49802124c211c895b69d
David Yulianto <grand_razor98@yahoo.com>
2024-12-21 19:57:05 +0700

    Update LoginPage.tsx

--- FE/src/pages/Auth/LoginPage.tsx
+++ FE/src/pages/Auth/LoginPage.tsx
@@ -66,6 +66,9 @@ const LoginPage = () => {
 						/>
 					</div>
 
+					<div className="flex justify-between w-full py-1">
+						<span className="text-sm text-[#BFCAD5] hover:text-[#92DFF3]">Forgot password</span>
+					</div>
 
 					<button
 						onClick={handleLogin}
